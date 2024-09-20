7a334db9fe055e8c2d9c9a0406ffc0ade388c6dd
xvin <xvin@bupt.edu.cn>
2024-09-20 18:05:34 +0800

    re-organize pages structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{html,ts}'],
+    content: ['./index.html', './src/**/*.{html,ts,js}', './app/**/*.{html,ts,js}'],
     theme: {
         extend: {
             colors: {
